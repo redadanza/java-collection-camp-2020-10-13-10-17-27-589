496036ffe811229d912c872b724c37edd810422c
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-14 23:06:01 +0800

    Java Collection- Raymond- Feature: answers

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import org.apache.commons.lang3.NotImplementedException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -12,14 +13,19 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return array.stream().filter(integer -> integer % 2 == 0).collect(Collectors.toList());
+        //throw new NotImplementedException();
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream().filter(integer -> integer % 3 == 0).collect(Collectors.toList());
+        // new NotImplementedException();
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+
+        //array.stream().filter(integer -> firstList.con)
+        return firstList.stream().filter(secondList::contains).collect(Collectors.toList());
+        //throw new NotImplementedException();
     }
 }
--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -2,7 +2,10 @@ package com.thoughtworks.collection;
 
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class Reduce {
 
@@ -13,14 +16,25 @@ public class Reduce {
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        //return arrayList.stream().collect(Collectors.averagingInt(Integer::intValue));
+        Integer result = arrayList.stream().reduce(0, (sum,current) -> { sum += current; return sum;});
+
+        return new BigDecimal(result).divide(new BigDecimal(arrayList.size())).doubleValue();
+
+
     }
 
     public int getMaxValue() {
-        throw new NotImplementedException();
+        //return arrayList.stream().reduce(0,(max, current)-> { if(max<current){ max = current; } return max;});
+        int maxed = arrayList.stream().reduce(0, (max, current) -> max > current ? max : current);
+        return arrayList.stream().reduce(0, Integer::max);
+
+
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        //.stream().filter(integer -> integer % 2 == 0).collect(Collectors.toList());
+      return arrayList.stream().filter(integer -> integer % 2 == 1).reduce(0,(odd, current) -> { odd = current; return odd;});
+
     }
 }
